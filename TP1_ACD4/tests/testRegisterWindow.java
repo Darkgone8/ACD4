2f52f3dd632453a4438ef34361042cb5c22404ef
SAUNIER DEBES Brice <brice.saunier@gmail.com>
2015-09-27 16:53:55 +0200

    Ajout de commentaires

--- TP1_ACD4/tests/testRegisterWindow.java
+++ TP1_ACD4/tests/testRegisterWindow.java
@@ -10,7 +10,10 @@ import registerWindow.RegisterWindow;
 public class testRegisterWindow {
   public static void main(String[] args) {
     RegisterWindow rw = null;
+
+    //Instanciation d’un register window
     System.out.println(rw.getInstance());
+    //Verification que l’on récupère la même instance que celle instaciée lors du dernier appel de la fonction
     System.out.println(rw.getInstance());
   }
 }
