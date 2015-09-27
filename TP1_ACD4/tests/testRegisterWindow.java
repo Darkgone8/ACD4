11c0137d828e0428ef700e6c8d6976ca9e4e7190
SAUNIER DEBES Brice <brice.saunier@gmail.com>
2015-09-27 16:56:20 +0200

    Desormai accède correctement à la variable static

--- TP1_ACD4/tests/testRegisterWindow.java
+++ TP1_ACD4/tests/testRegisterWindow.java
@@ -9,11 +9,10 @@ import registerWindow.RegisterWindow;
 
 public class testRegisterWindow {
   public static void main(String[] args) {
-    RegisterWindow rw = null;
 
     //Instanciation d’un register window
-    System.out.println(rw.getInstance());
+    System.out.println(RegisterWindow.getInstance());
     //Verification que l’on récupère la même instance que celle instaciée lors du dernier appel de la fonction
-    System.out.println(rw.getInstance());
+    System.out.println(RegisterWindow.getInstance());
   }
 }
