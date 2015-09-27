dff560d27bd9b8c978894d4695a0f52cc5258959
SAUNIER DEBES Brice <brice.saunier@gmail.com>
2015-09-27 17:09:25 +0200

    Ajout de commentaires

--- TP1_ACD4/tests/testFabriqueInterfaceGraphique.java
+++ TP1_ACD4/tests/testFabriqueInterfaceGraphique.java
@@ -12,9 +12,13 @@ import interfaceGraphique.InterfaceGraphiqueWindows;
 
 public class testFabriqueInterfaceGraphique {
   public static void main(String[] args) {
+    //Instanciation d’une fabrique de chaque type
     FabriqueInterfaceGraphique IGWindows = new InterfaceGraphiqueWindows();
     FabriqueInterfaceGraphique IGLinux = new InterfaceGraphiqueLinux();
     FabriqueInterfaceGraphique IGIOS = new InterfaceGraphiqueIOS();
+
+
+    //Les valeurs sont attribuée, et sans faire de distinction selon le type
 
     IGWindows.creerBarre("rouge", 12);
     IGWindows.creerBouton("bleu", 22, 5);
@@ -24,7 +28,7 @@ public class testFabriqueInterfaceGraphique {
     IGLinux.creerBouton("noir",89,999);
     IGLinux.creerMenu("blanc", 222222);
 
-    IGIOS.creerBarre("jaune", 012);
+    IGIOS.creerBarre("jaune", 12);
     IGIOS.creerBouton("noir", 218, 398);
     IGIOS.creerMenu("bleu", 1789);
   }
